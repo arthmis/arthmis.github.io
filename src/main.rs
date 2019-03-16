bd926c503ba7f47d62ab5c28ff328ef1fa960658
lazypassion <25536767+lazypassion@users.noreply.github.com>
2019-03-15 23:58:03 -0400

    added basic server to serve static files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use actix_web::server;
+use actix_web::{fs, App};
+
 fn main() {
-    println!("Hello, world!");
+    let _server = server::new(|| {
+        App::new()
+            .handler(
+                "/",
+                fs::StaticFiles::new("frontend/")
+                    .expect("couldn't find frontend folder")
+                    .index_file("index.html")
+            )
+            .finish()
+    })
+    .bind("127.0.0.1:8000")
+    .expect("couldn't bind to port")
+    .run();
 }
